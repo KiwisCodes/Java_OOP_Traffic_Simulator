6602e28f71049f52dd1f15b6781a48305d2124a9
KiwisCodes <phanthanhhung.subscription@gmail.com>
2025-12-14 10:51:41 +0100

    Add doc/ and target/ to .gitignore

--- doc/package-search-index.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"view"}];updateSearchResults();
-=======
-packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"data"},{"l":"model"},{"l":"model.infrastructure"},{"l":"model.vehicles"},{"l":"testjava"},{"l":"util"},{"l":"view"}];updateSearchResults();
->>>>>>> minh_khoi
